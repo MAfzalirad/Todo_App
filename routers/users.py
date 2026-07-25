d16511fcc089d63ffb150891ae245180a08b1fa6
mohammadrad <mohammadafzalirad2001@gmail.com>
2026-07-25 18:19:35 +0330

    Modification in Users.py

--- routers/users.py
+++ routers/users.py
@@ -27,6 +27,11 @@ user_dependency = Annotated[dict, Depends(get_current_user)]
 
 bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated = 'auto')
 
+class UserVerification(BaseModel):
+    password: str
+    new_password: str = Field(min_length=6)
+
+
 
 @router.get('/', status_code=status.HTTP_200_OK)
 async def get_user_info(user: user_dependency, db: db_dependency):
@@ -34,14 +39,14 @@ async def get_user_info(user: user_dependency, db: db_dependency):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could Not Authenticate User')
     return db.query(Users).filter(Users.id == user.get('id')).first()
 
-@router.post('/password/{new_passwoed}', status_code=status.HTTP_202_ACCEPTED)
-async def change_password(user: user_dependency,db: db_dependency, new_password):
+@router.post('/password', status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user: user_dependency,db: db_dependency,user_verification: UserVerification):
     if user is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Could Not Authenticate User')
     requested_user= db.query(Users).filter(Users.id == user.get('id')).first()
-    if requested_user is None:
-         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='User was not found')
-    requested_user.hash_password = bcrypt_context.hash(new_password)
+    if not bcrypt_context.verify(user_verification.new_password, requested_user.hash_password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Wrong Password ')
+    requested_user.hash_password = bcrypt_context.hash(user_verification.new_password)
 
     db.add(requested_user)
     db.commit()
