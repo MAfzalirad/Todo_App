cb1cc693c4e3f3efcbbd07a2eaa385dc2215f09c
mohammadrad <mohammadafzalirad2001@gmail.com>
2026-08-04 23:14:43 +0330

    add edit todo page

--- routers/todos.py
+++ routers/todos.py
@@ -68,6 +68,23 @@ async def render_add_todo_page(request: Request):
         return redirect_to_login()
 
 
+@router.get('/edit-todo-page/{todo_id}')
+async def render_edit_todo_page(request: Request, db: db_dependency, todo_id: int = Path(gt=0)):
+    try:
+        user = await get_current_user(request.cookies.get('access_token'))
+        if user is None:
+            return redirect_to_login()
+
+        todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).first()
+        if todo_model is None:
+            raise HTTPException(status_code=404, detail='Todo not found.')
+
+        return templates.TemplateResponse(request, 'edit-todo.html', {'todo': todo_model, 'user': user})
+
+    except:
+        return redirect_to_login()
+
+
 ### Endpoint
 @router.get('/', status_code=status.HTTP_200_OK)
 async def read_all(user: user_dependency, db: db_dependency):
